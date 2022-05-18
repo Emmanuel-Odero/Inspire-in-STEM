a8cb4b704b12d4841388dec9f5f4289a8865d811
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-05-18 11:30:16 +0300

    lesson5.py data methods

---
+++ lesson5.py
@@ -0,0 +1,13 @@
+# methods
+# name = "Mathenge Mugo"
+name = "Ada Lovelace"
+user_name= " Lovelace "
+age = 15
+# age = 55
+# person = "I am "+ str(name)+ " and my age is " + str(age)
+# print ("I am "+ str(name)+ " and my age is " + str(age))
+# the format() method
+#print("My name is {} and I am {} years old ".format(name, age))
+# newline \n and tab \t
+# print (f"My \t name is {name} \n and I am {age} years old")
+print(user_name.lstrip())

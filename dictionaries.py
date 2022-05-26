b74018f5788407fb9a07f16ff1f0724601a96beb
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-05-26 10:18:53 +0300

    get method for accesing data dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,13 +1,13 @@
-#A dictionary is collection of key value pair. key:value
-#syntax: dictionary = {'key':'value'}
+#-----A dictionary is collection of key value pair. key:value
+#----syntax: dictionary = {'key':'value'}
 # names =['john', 'Mary']
 colors= {'color':'red'}
 vehicle= {'type':'toyota','plate_number':'KYZ45'}
 # print(type(names)) #we use the type method to read the data type of a string
-#Acessing values in a dictionary
+#----Acessing values in a dictionary
 # print(vehicle['type'])#You can acces the value of an element inside a dictionary using the key
 #print vehicle type and the plate number
-print(vehicle['type'],vehicle['plate_number'])
+#print(vehicle['type'],vehicle['plate_number'])
 #Dictionary Person
 person = {
     'name':'Joan',
@@ -15,9 +15,10 @@ person = {
     'Phone_number':'997765',
     'location':'Dagoretti'
     }
-person['age']='21'
 #print(type(person))
 #print(person)
-#looping over dictionaties
-for key, value in person.items():
-    print(f"{key}:{value}")
+#------looping over dictionaties
+# for key, value in person.items():
+#     print(f"{key}:{value}")
+print(person.get("password','the 'location' key is non-existent"))
+#Using get to access the value in a dictionary
---
+++ login.py
@@ -0,0 +1,8 @@
+person ={
+    'user_name':'lutherKing',
+    'email':'luther@king.com',
+    'password':'luther123'
+     }
+ask_user_name = input('entre your user name')
+ask_user_email = input('entre your user email')
+ask_user_password = input('entre your user password')

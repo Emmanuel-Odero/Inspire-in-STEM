913a5bfd6eae3cff7212882669ee5de3667ec635
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-05-27 09:20:35 +0300

    working on python challanges

---
+++ finding_even.py
@@ -0,0 +1,9 @@
+from ast import If
+
+
+num = 7
+operate = num%2
+if(operate==0):
+    print('the number is even')
+else:
+    print('the number is odd')
---
+++ lesson10.py
@@ -0,0 +1,7 @@
+mary_fav_food = ['beef','chicken','vegetable']
+jane_fav_food = ['rice', 'ugali', 'potatoe']
+#dictionary containing the above
+food ={
+    'mary':[],
+    'jane':[],
+}

8a44a0c5419615e5f0ee10b4458d77b9afe7910f
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-06-21 20:20:24 +0300

    blinking single led light with pico

---
+++ arduino-rasberry/pico/oled1/blinking_led_with_button.py
@@ -0,0 +1,9 @@
+from machine import Pin
+import time
+
+led = Pin(14, Pin.OUT)
+button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+while True:
+    if button.value():
+        led.toggle()
+        time.sleep(0.5)

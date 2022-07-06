6e956eacba8b8ca9134a20440edfeac8dc3fee75
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-07-06 10:51:46 +0300

    learning about Javascript

---
+++ web/js/index.js
@@ -0,0 +1,12 @@
+let message = "Hello World"
+console.log(message)
+//Single line comment in Javascript
+/*
+Multi-line comment in Javascript
+ */
+let num1 = 134
+let num2 = 134
+
+function submitButton(){
+    alert("Button has been clicked")
+}
